c1969e6ec2a941fe446f18121bae714810313b6c
FedericaMarini <federica.ita46@hotmail.it>
2017-02-17 18:47:10 +0100

    Primo commit

---
+++ app/src/main/java/com/example/federicamarini/testtabs/OneFragment.java
@@ -0,0 +1,52 @@
+package com.example.federicamarini.testtabs;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link OneFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link OneFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class OneFragment extends Fragment {
+    private Button startButton;
+
+
+    public OneFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_one, container, false);
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+}
---
+++ app/src/main/java/com/example/federicamarini/testtabs/SecondActivity.java
@@ -0,0 +1,15 @@
+package com.example.federicamarini.testtabs;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Federica Marini on 17/02/2017.
+ */
+public class SecondActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
